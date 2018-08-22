c6dda561fd7277042dbbbb538cb9ec71fe953142
whisperingpixel <martin.sudmanns@sbg.ac.at>
2018-08-22 12:06:39 +1000

    added demo script

---
+++ demo.py
@@ -0,0 +1,31 @@
+import rhino
+import datetime
+import png
+
+f = open("demo-run.txt","w")
+f.write("Demo-data execution start: ")
+f.write(str(datetime.datetime.now()) + "\n")
+
+f.write("1: Load demo dataset ...")
+dc = rhino.Datacube(show_progress = True)
+dc.load('demodata/demolayer_lowres.tif', None)
+f.write("done\n")
+
+
+f.write("2: Aggregation based on colour information ...")
+level = dc.aggregate(0, {"key":"class","value":1, "operator": "eq"})
+f.write("done\n")
+coverage = dc.getCoverageView(level["depth"])
+png.from_array(coverage.getStupidArray().tolist(), 'L').save("result-level-"+str(level["depth"])+".png")
+
+f.write("3: Selection based on shape information ....")
+objects, coverage, level = dc.selectObjectsByCondition(level["depth"],{"key": "compactness", "value": 0.5, "operator": "gt"}, create_level=True)
+f.write("done\n")
+png.from_array(coverage.getStupidArray().tolist(), 'L').save("result-level-"+str(level["depth"])+".png")
+f.write("Demo-data execution finished.")
+
+f.close()
+
+
+
+
